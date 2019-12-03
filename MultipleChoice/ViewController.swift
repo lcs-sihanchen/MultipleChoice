314171e92372cd617642ee6d4f78c21e48cc3085
Chen Sihan <schen@lcs.on.ca>
2019-12-03 15:03:28 -0500

    Initial Commit

---
+++ MultipleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleChoice
+//
+//  Created by Chen, Sihan on 2019-12-03.
+//  Copyright © 2019 Chen, Sihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
