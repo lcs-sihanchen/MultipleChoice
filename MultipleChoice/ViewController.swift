bab15d6611e34fafaaae8a40933ccb45e396010f
Chen Sihan <schen@lcs.on.ca>
2019-12-08 15:46:57 -0500

    Explaining the guard statements

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -20,32 +20,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        outputMessage.text = ""
     }
     
     // MARK: Methods
     @IBAction func checkStudentAnswers(_ sender: Any) {
         // Avoid false input
+        // Avoid empty text
         guard let questionNumberInputAsString = questionNumber.text, questionNumberInputAsString != "" else {
             outputMessage.text = "Please enter an integer value greater than zero."
             return
         }
         
+        // Avoid decimals, set the range of the numbers
         guard let questionNumberInputAsInt = Int(questionNumberInputAsString), questionNumberInputAsInt > 0 else {
             outputMessage.text = "Please enter an integer value greater than zero."
             return
         }
-        
+        // Avoid empty text for correct answer
+        // Word count has to match question number input
         guard let correctAnswerInput = correctAnswer.text, correctAnswerInput != "", correctAnswerInput.count == questionNumberInputAsInt else {
             outputMessage.text = "Please be sure you input exactly \(questionNumberInputAsInt) answers for the answer key."
             return
         }
-        
-        guard let studentAnswer = studentAnswerInput.text, studentAnswer != "", studentAnswer.count == questionNumberInputAsInt, studentAnswer.contains("A") || studentAnswer.contains("B") || studentAnswer.contains("C") || studentAnswer.contains("D") || studentAnswer.contains("E") else {
+        // Avoid empty text for student answer
+        // Word count has to match question number input
+        guard let studentAnswer = studentAnswerInput.text, studentAnswer != "", studentAnswer.count == questionNumberInputAsInt else {
             outputMessage.text = "Please be sure you input exactly \(questionNumberInputAsInt) answers for student answers."
             return
         }
-         let possibleCharacter = "ABCDE"
-        
+        // Make sure only "ABCDE" are used
+        let possibleCharacter = "ABCDE"
+        // Use for...in loop to avoid user using letters other than ABCDE
         for characters in studentAnswer {
             guard possibleCharacter.contains(characters) else {
                 outputMessage.text = "Student answers contain invalid choices, please ensure only character A, B, C, D or E are used."
@@ -53,18 +59,20 @@ class ViewController: UIViewController {
             }
             
         }
+        // Use for...in loop to avoid user using letters other than ABCDE
         for characters in correctAnswerInput {
-           
+            
             guard possibleCharacter.contains(characters) else {
                 outputMessage.text = "Correct answers contain invalid choices, please ensure only character A, B, C, D or E are used."
                 return
             }
             
-            
         }
         
         
     }
-
-
+    
+    
+    
+    
 }
